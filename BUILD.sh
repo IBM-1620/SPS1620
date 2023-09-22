40d0761a6252cc791a23a81712725fa5adabf422
Dave Babcock <dave@babcock-family.org>
2023-09-21 22:30:39 -0700

    Cleanup build

--- BUILD.sh
+++ BUILD.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 rm -rf SPS1620.jar *.class SPS1620
-/cygdrive/c/Program\ Files/Java/jdk1.8.0_333/bin/javac *.java
+javac *.java
 mkdir SPS1620
 mv *.class SPS1620
-/cygdrive/c/Program\ Files/Java/jdk1.8.0_333/bin/jar cfm SPS1620.jar MANIFEST.MF SPS1620
+jar cfm SPS1620.jar MANIFEST.MF SPS1620
